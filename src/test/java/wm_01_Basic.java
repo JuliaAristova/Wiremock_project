352bd3fcd59a353c9da742e7bac2c63ad12b2f2f
juliaAr <yuliyaaristova@gmail.com>
2025-06-25 09:57:23 -0400

    Basic request - status code, header

---
+++ src/test/java/wm_01_Basic.java
@@ -0,0 +1,50 @@
+/*
+1. Start wiremock server: java -jar wiremock-standalone-3.13.1.jar
+2. Add req response mapping: C:\Users\julia\Documents\API\mapping
+3. Run the test
+ */
+
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class wm_01_Basic  {
+
+    @Test
+    public void test_1() {
+                given()
+                .get("http://localhost:8080/users/1")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void test_2() {
+                given()
+                .get("http://localhost:8080/users/2")
+                .then()
+                .assertThat()
+                .statusCode(201);
+    }
+
+    @Test
+    public void test_3() {
+
+       String contentType =
+                given()
+                .get("http://localhost:8080/users/3")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .header("Content-Type");
+
+       System.out.println("Test response header Content-Type: " + contentType);
+
+       Assert.assertEquals(contentType, "text/plain");
+    }
+}
